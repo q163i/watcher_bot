e88f722894b71dcc99f185b9e35e278e27867e47
Roman Litvinov <roman.o.litvinov@gmail.com>
2024-03-11 20:54:50 -0400

    MAR-11: Add new commands

--- watcher_bot.py
+++ watcher_bot.py
@@ -1,17 +1,25 @@
 import os
 import telebot
-import time
 import argparse
 import sys
+import subprocess
+from threading import Timer
+from datetime import datetime
+import ssl
+from urllib.parse import urlparse
+import socket
+import logging
 
 TOKEN = os.getenv("TOKEN")
 ALLOWED_USERS = [int(user_id) for user_id in os.getenv("ALLOWED_USERS", "").split(",") if user_id]
 
 if not TOKEN or not ALLOWED_USERS:
-    print("Error: TOKEN and ALLOWED_USERS cannot be empty.")
+    logging.error("Error: TOKEN and ALLOWED_USERS cannot be empty.")
     sys.exit(1)
 
 bot = telebot.TeleBot(TOKEN)
+
+logging.basicConfig(filename='/tmp/watcher_bot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def send_message(chat_id, text):
     bot.send_message(chat_id, text)
@@ -42,20 +50,94 @@ def run_bot():
             try:
                 bot.polling(none_stop=True)
             except Exception as e:
-                print(f"ERROR: {e}")
+                logging.error(f"Error: {e}")
                 time.sleep(10)
     except Exception as e:
-        print(f"ERROR: {e}")
+        logging.error(f"Error: {e}")
         time.sleep(10)
+
+def send_help_message(chat_id):
+    help_text = "/help - Show available commands\n"
+    help_text += "/checkNetwork <server> <port> - Check network availability to the specified server and port (8.8.8.8 443)\n"
+    help_text += "/checkCertificate <domain_name> - Check SSL certificate expiration date for the specified domain (https://google.com)\n"
+    help_text += "\n"
+    help_text += "Command executed from the server: python3 watcher_bot.py getData --data /path/to/file.exe - send a file from the server to Telegram\n"
+    send_message(chat_id, help_text)
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    if is_allowed_user(message.chat.id):
+        send_help_message(message.chat.id)
+
+@bot.message_handler(commands=['checkNetwork'])
+def handle_check_network(message):
+    if is_allowed_user(message.chat.id):
+        try:
+            _, server, port = message.text.split()
+
+            # Run the network check command with a timeout of 3 seconds
+            process = subprocess.Popen(['nc', '-vnz', '-w', '3', server, port], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            timer = Timer(3, process.kill)
+
+            try:
+                timer.start()
+                stdout, stderr = process.communicate()
+            finally:
+                timer.cancel()
+
+            if process.returncode == 0:
+                send_message(message.chat.id, f"[System] Network check result: Port {port} is available on {server}")
+            else:
+                send_message(message.chat.id, f"[System] Network check result: Port {port} is not available on {server}")
+        except ValueError:
+            send_message(message.chat.id, "[System] Incorrect command format. Use /checkNetwork <server> <port>")
+    else:
+        forward_spam_message(message)
+
+@bot.message_handler(commands=['checkCertificate'])
+def handle_check_certificate(message):
+    if is_allowed_user(message.chat.id):
+        try:
+            _, domain = message.text.split()
+            expiration_days = get_certificate_expiration_days(domain)
+            if expiration_days is not None:
+                send_message(message.chat.id, f"[System] SSL certificate for {domain} expires in {expiration_days} days.")
+            else:
+                send_message(message.chat.id, f"[System] Unable to retrieve SSL certificate information for {domain}.")
+        except ValueError:
+            send_message(message.chat.id, "[System] Incorrect command format. Use /checkCertificate <domain_name>")
+    else:
+        forward_spam_message(message)
+
+def forward_spam_message(message):
+    for user_id in ALLOWED_USERS:
+        if user_id != message.chat.id:
+            send_message(user_id, f"[Spam] Message from unauthorized user (ID: {message.chat.id}): {message.text}")
+
+def get_certificate_expiration_days(domain):
+    try:
+        url = urlparse(domain)
+        hostname = url.netloc
+
+        context = ssl.create_default_context()
+        with context.wrap_socket(socket.create_connection((hostname, 443)), server_hostname=hostname) as sock:
+            cert = sock.getpeercert()
+
+        expiration_date = datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+        days_until_expiration = (expiration_date - datetime.utcnow()).days
+        return days_until_expiration
+    except Exception as e:
+        logging.error(f"Error retrieving SSL certificate information: {e}")
+        return None
 
 if __name__ == '__main__':
     args = handle_command_line_args()
 
     if args.command == 'getData' and args.data:
         for user_id in ALLOWED_USERS:
-            send_message(user_id, "[System] python3 watcher_bot.py getData --data * complete! ")
+            send_message(user_id, "[System] python3 watcher_bot.py getData --data * completed! ")
             send_file(user_id, args.data)
     elif args.command == 'run':
         run_bot()
     else:
-        print("Invalid command. Usage: python3 watcher_bot.py [getData --data /path/file.env | run]")
+        logging.error("Invalid command. Usage: python3 watcher_bot.py [getData --data /path/to/file.env | run]")
